145aaaed705fdef99344171ae5c8c2e3869f3c6d
Chris Nickerson <cnickerson2@studentmail.nait.ca>
2017-11-23 13:26:19 -0700

    Added EmployeeController

---
+++ Bikes/BikesSystem/BLL/EmployeeController.cs
@@ -0,0 +1,21 @@
+﻿using BikesData.Entities;
+using BikesSystem.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BikesSystem.BLL
+{
+    public class EmployeeController
+    {
+        public Employee Employee_Get(int employeeID)
+        {
+            using (var context = new EBikesContext())
+            {
+                return context.Employees.Find(employeeID);
+            }
+        }
+    }
+}
