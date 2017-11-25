72eb8d34d580dec943b3cba388a5d95ddbb7ebf4
tblakely2 <tblakely2@studentmail.nait.ca>
2017-11-25 14:34:37 -0700

    Created JobController and CurrentJob POCO

---
+++ Bikes/BikesData/POCOs/CurrentJob.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BikesData.POCOs
+{
+    public class CurrentJob
+    {
+        public int JobID { get; set; }
+        public DateTime InDate { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? DoneDate { get; set; }
+        public string CustomerName { get; set; }
+        public string Phone { get; set; }
+    }
+}
---
+++ Bikes/BikesSystem/BLL/JobController.cs
@@ -0,0 +1,41 @@
+﻿using BikesData.Entities;
+using BikesData.POCOs;
+using BikesSystem.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BikesSystem.BLL
+{
+    public class JobController
+    {
+        public List<Job> Job_List()
+        {
+            using (var context = new EBikesContext())
+            {
+                return context.Jobs.ToList();
+            }
+        }
+
+        public List<CurrentJob> CurrentJob_List()
+        {
+            using (var context = new EBikesContext())
+            {
+                var results = from x in context.Jobs
+                              where x.JobDateOut == null
+                              select new CurrentJob
+                              {
+                                  JobID = x.JobID,
+                                  InDate = x.JobDateIn,
+                                  StartDate = x.JobDateStarted,
+                                  DoneDate = x.JobDateDone,
+                                  CustomerName = x.Customer.LastName + ", " + x.Customer.FirstName,
+                                  Phone = x.Customer.ContactPhone
+                              };
+                return results.ToList();
+            }
+        }
+    }
+}
