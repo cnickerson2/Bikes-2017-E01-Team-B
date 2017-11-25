43f607985c8eab0956dcb5afe3a7e1eeeb7581d9
tblakely2 <tblakely2@studentmail.nait.ca>
2017-11-25 14:42:13 -0700

    Created StandardJobController

---
+++ Bikes/BikesSystem/BLL/StandardJobController.cs
@@ -0,0 +1,21 @@
+﻿using BikesData.Entities;
+using BikesSystem.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BikesSystem.BLL
+{
+    public class StandardJobController
+    {
+        public StandardJob StandardJobInformation_Get(int standardjobid)
+        {
+            using (var context = new EBikesContext())
+            {
+                return context.StandardJobs.Find(standardjobid);
+            }
+        }
+    }
+}
