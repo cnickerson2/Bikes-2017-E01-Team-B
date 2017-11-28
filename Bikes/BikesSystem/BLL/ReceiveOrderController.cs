c677ff60a14a1d4d279bbf04eb007cbd499a8247
Chris Nickerson <cnickerson2@studentmail.nait.ca>
2017-11-27 19:25:08 -0700

    Adding necessary BLLs for Receiving

---
+++ Bikes/BikesSystem/BLL/ReceiveOrderController.cs
@@ -0,0 +1,17 @@
+﻿using BikesData.Entities;
+using BikesSystem.DAL;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BikesSystem.BLL
+{
+    [DataObject]
+    public class ReceiveOrderController
+    {
+
+    }
+}
---
+++ Bikes/BikesSystem/BLL/ReturnOrderDetailController.cs
@@ -0,0 +1,16 @@
+﻿using BikesData.Entities;
+using BikesSystem.DAL;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BikesSystem.BLL
+{
+    [DataObject]
+    public class ReturnOrderDetailController
+    {
+    }
+}
