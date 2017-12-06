38a268a7a7a68f3a8a9b2ac96fd6746a25bc22f8
Chris Nickerson <cnickerson2@studentmail.nait.ca>
2017-12-06 14:44:55 -0700

    Moved transaction to single method
    As per don's comments

--- Bikes/Website/Receiving/Receiving.aspx.cs
+++ Bikes/Website/Receiving/Receiving.aspx.cs
@@ -4,6 +4,7 @@ using BikesSystem.BLL;
 using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Data.Entity.Validation;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -36,83 +37,48 @@ public partial class Receiving_Receiving : System.Web.UI.Page
 
     protected void ReceiveBtn_Command(object sender, CommandEventArgs e)
     {
-        //Create a new Receive Order
-        ReceiveOrderController receiveordersysmgr = new ReceiveOrderController();
-        ReceiveOrder newReceiveOrder = new ReceiveOrder
+        try
         {
-            PurchaseOrderID = int.Parse((SelectedOrderFormView.FindControl("PurchaseOrderID_FormView") as Label).Text),
-            ReceiveDate = DateTime.UtcNow
-        };        
-        int receiveOrderID = receiveordersysmgr.ReceiveOrder_ReceiveNewOrder(newReceiveOrder);
-        bool isOrderCompleted = true;
-        foreach (GridViewRow row in OutstandingDetailsGridView.Rows)
-        {
-            OutstandingPurchaseOrderDetails outDetails = new OutstandingPurchaseOrderDetails();
-
-            outDetails.PurchaseOrderDetailID = int.Parse((row.FindControl("PurchaseOrderDetailID") as Label).Text);
-            outDetails.PurchaseOrderID = int.Parse((row.FindControl("PurchaseOrderID") as Label).Text);
-            outDetails.PartID = int.Parse((row.FindControl("PartID") as Label).Text);
-            outDetails.PartDescription = (row.FindControl("PurchaseOrderDetailID") as Label).Text;
-            outDetails.QuantityOnOrder = int.Parse((row.FindControl("QuantityOnOrder") as Label).Text);
-            outDetails.QuantityOutstanding = int.Parse((row.FindControl("QuantityOutstanding") as Label).Text);
-            outDetails.ReceivingAmount = int.Parse((row.FindControl("ReceivingAmount") as TextBox).Text);
-            outDetails.ReturningAmount = int.Parse((row.FindControl("ReturningAmount") as TextBox).Text);
-            outDetails.ReturningReason = (row.FindControl("ReturningReason") as TextBox).Text;
-            outDetails.VendorPartNumber = (row.FindControl("VendorPartNumber") as Label).Text;
-            if(outDetails.ReceivingAmount > 0)
+            List<OutstandingPurchaseOrderDetails> outDetails = new List<OutstandingPurchaseOrderDetails>();
+            foreach (GridViewRow row in OutstandingDetailsGridView.Rows)
             {
-                //New ReceiveOrderDetail made for each part received
-                ReceiveOrderDetailController receiveorderdetailsysmgr = new ReceiveOrderDetailController();
-                ReceiveOrderDetail newReceiveOrderDetail = new ReceiveOrderDetail();
-                newReceiveOrderDetail.ReceiveOrderID = receiveOrderID;
-                newReceiveOrderDetail.PurchaseOrderDetailID = outDetails.PurchaseOrderDetailID;
-                newReceiveOrderDetail.QuantityReceived = outDetails.ReceivingAmount;
-                receiveorderdetailsysmgr.ReceiveOrderDetail_ReceiveNewOrderItem(newReceiveOrderDetail);
-
-                //Increase QuantityOnHand of Part
-                PartController partsysmgr = new PartController();
-                Part receivedPart = partsysmgr.Part_Get(outDetails.PartID);
-                receivedPart.QuantityOnHand += outDetails.ReceivingAmount;
-                //Reduce QuantityOnOrder of Part
-                receivedPart.QuantityOnOrder -= outDetails.ReceivingAmount;
-                //Update the Part
-                partsysmgr.Part_Update(receivedPart);
-            }
-            if(outDetails.ReturningAmount > 0)
-            {
-                PartController partsysmgr = new PartController();
-                Part receivedPart = partsysmgr.Part_Get(outDetails.PartID);
-
-                //ReturnOrderDetail made for each returning item
-                ReturnOrderDetailController returnorderdetailsysmgr = new ReturnOrderDetailController();
-                ReturnedOrderDetail newReturnOrderDetail = new ReturnedOrderDetail
+                OutstandingPurchaseOrderDetails outDetail = new OutstandingPurchaseOrderDetails
                 {
-                    ReceiveOrderID = receiveOrderID,
-                    PurchaseOrderDetailID = outDetails.PurchaseOrderDetailID,
-                    ItemDescription = outDetails.PartDescription,
-                    Quantity = outDetails.ReturningAmount,
-                    Reason = outDetails.ReturningReason,
-                    VendorPartNumber = outDetails.VendorPartNumber
+                    PurchaseOrderDetailID = int.Parse((row.FindControl("PurchaseOrderDetailID") as Label).Text),
+                    PurchaseOrderID = int.Parse((row.FindControl("PurchaseOrderID") as Label).Text),
+                    PartID = int.Parse((row.FindControl("PartID") as Label).Text),
+                    PartDescription = (row.FindControl("PurchaseOrderDetailID") as Label).Text,
+                    QuantityOnOrder = int.Parse((row.FindControl("QuantityOnOrder") as Label).Text),
+                    QuantityOutstanding = int.Parse((row.FindControl("QuantityOutstanding") as Label).Text),
+                    ReceivingAmount = int.Parse((row.FindControl("ReceivingAmount") as TextBox).Text),
+                    ReturningAmount = int.Parse((row.FindControl("ReturningAmount") as TextBox).Text),
+                    ReturningReason = (row.FindControl("ReturningReason") as TextBox).Text,
+                    VendorPartNumber = (row.FindControl("VendorPartNumber") as Label).Text
                 };
+                outDetails.Add(outDetail);
             }
-            if(outDetails.QuantityOutstanding - outDetails.ReceivingAmount > 0)
-            {
-                isOrderCompleted = false;
-            }
+            PurchaseOrderController sysmgr = new PurchaseOrderController();
+            sysmgr.PurchaseOrder_ReceiveOrder(outDetails);
+
+            DataBind();
         }
-
-        //Check to see if there's anything in the Cart
-        //TODO: Add the cart
-
-        //Complete the order
-        if(isOrderCompleted)
+        catch (DbEntityValidationException ex)
         {
-            PurchaseOrderController purchaseordersysmgr = new PurchaseOrderController();
-            purchaseordersysmgr.PurchaseOrder_CloseOrder((int.Parse((SelectedOrderFormView.FindControl("PurchaseOrderID_FormView") as Label).Text)));
+            foreach (var eve in ex.EntityValidationErrors)
+            {
+                foreach (var ve in eve.ValidationErrors)
+                {
+                    Message.Text += string.Format("Error: \"{0}\" <br />",
+                        ve.ErrorMessage);
+                }
+            }
         }
-
-        DataBind();
     }
 
-   
+
+
+    protected void ReturningAmount_TextChanged(object sender, EventArgs e)
+    {
+        UnorderedCartListView.Visible = true;
+    }
 }
