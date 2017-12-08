6ccb9836f150d700139cb93e7a4500a6be29920f
Colton Loewen <loewencjay@gmail.com>
2017-12-07 20:21:37 -0700

    Implemented checkout view

---
+++ Bikes/BikesData/DTOs/OnlineShoppingCart.cs
@@ -0,0 +1,16 @@
+﻿using BikesData.POCOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BikesData.DTOs
+{
+    public class OnlineShoppingCart
+    {
+        public decimal Total { get; set; }
+        public DateTime LastUpdated { get; set; }
+        public List<OnlineShoppingCartItem> Parts { get; set; }
+    }
+}
---
+++ Bikes/BikesData/POCOs/OnlineShoppingCartItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BikesData.POCOs
+{
+    public class OnlineShoppingCartItem
+    {
+        public string Description { get; set; }
+        public int Quantity { get; set; }
+        public int QuantityOnHand { get; set; }
+        public decimal Price { get; set; }
+        public DateTime LastUpdated { get; set; }
+    }
+}
